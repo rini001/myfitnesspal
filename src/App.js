a84f02c2b332b891615ff9c559b7479d6424041b
rini960 <debnathrenaissance95@gmail.com>
2022-04-05 21:43:37 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import { Head } from './Components/Head';
-import {Navbar} from './Components/Navbar';
-import {About} from './Components/About';
+import { MarketPlace } from "./Components/AppPage/MarketPlace";
+import { FeaturedApps } from "./Components/AppPage/FeaturedApps";
+import { Slides } from "./Components/AppPage/Slides";
+import { Sidebar } from "./Components/AppPage/Sidebar";
+import "./App.css";
+// import { Head } from './Components/Head';
+// import {Navbar} from './Components/Navbar';
+// import {About} from './Components/About';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      {/* <Navbar/>
      <Head/>
-     <About/>
+     <About/> */}
+      <Slides />
+      <Sidebar />
+      <FeaturedApps />
+
+      <MarketPlace />
     </div>
   );
 }
