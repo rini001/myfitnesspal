5d672ced3399d570e550016bbf23f6aa6a4a3f07
@NitinMaharshi <nitin77n@gmail.com>
2022-04-08 14:42:51 +0530

    add sign up page stage7

---
+++ src/Components/SignUP/Stage7th.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+const Stage7th = () => {
+  return (
+    <>
+      <section className={`${styles.MuiBox_root} ${styles.css_1fxczvu}`}>
+        <span className={styles.span1}>
+          <img
+            alt="background"
+            sizes="100vw"
+            src="https://www.myfitnesspal.com/_next/image?url=%2Fpages%2Fsignup%2Fbg.png&w=2048&q=75"
+            decoding="async"
+            data-nimg="fill"
+            className={styles.img1}
+          />
+        </span>
+        <div
+          className={`${styles.MuiContainer_root} ${styles.MuiContainer_maxWidthSm} ${styles.css_qpa126}`}
+        >
+          <main className={`${styles.MuiBox_root} ${styles.css_dmeihm}`}>
+            <div className={`${styles.MuiBox_root} ${styles.css_1t5i1n4}`}>
+              <form className={styles.section3}>
+                <div
+                  className={`${styles.MuiGrid_root} ${styles.MuiGrid_container} ${styles.MuiGrid_spacing_xs_8} ${styles.MuiGrid_direction_xs_column} ${styles.css_1vi05hx}`}
+                >
+                  <div
+                    className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.css_1wxaqej}`}
+                  >
+                    <h1
+                      className={`${styles.MuiTypography_root} ${styles.MuiTypography_h3} ${styles.MuiTypography_gutterBottom} ${styles.css_bwtxxq}`}
+                    >
+                      Create a username.
+                    </h1>
+                  </div>
+                  <div
+                    className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.css_1wxaqej}`}
+                  >
+                    <div
+                      className={`${styles.MuiFormControl_root} ${styles.MuiFormControl_fullWidth} ${styles.MuiTextField_root} ${styles.css_1ojbn3v}`}
+                    >
+                      <label
+                        className={`${styles.MuiInputLabel_root} ${styles.MuiInputLabel_formControl} ${styles.MuiInputLabel_animated} ${styles.MuiInputLabel_shrink} ${styles.MuiInputLabel_outlined} ${styles.MuiFormLabel_root} ${styles.MuiFormLabel_colorPrimary} ${styles.MuiFormLabel_filled} ${styles.css_1f09bpi}`}
+                        for="Create a username"
+                        id="Create a username-label"
+                      >
+                        Create a username
+                      </label>
+                      <div
+                        className={`${styles.MuiOutlinedInput_root} ${styles.MuiInputBase_root} ${styles.MuiInputBase_colorPrimary} ${styles.MuiInputBase_fullWidth} ${styles.MuiInputBase_formControl} ${styles.css_1v3dwsv}`}
+                      >
+                        <input
+                          aria-invalid="false"
+                          id="Create a username"
+                          type="text"
+                          className={`${styles.MuiOutlinedInput_input} ${styles.MuiInputBase_input} ${styles.css_5v3ygq}`}
+                          value=""
+                        />
+                        <fieldset
+                          aria-hidden="true"
+                          className={`${styles.MuiOutlinedInput_notchedOutline} ${styles.css_igs3ac}`}
+                        >
+                          <legend className={`${styles.css_1z7n62}`}>
+                            <span>Create a username</span>
+                          </legend>
+                        </fieldset>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.css_1wxaqej}`}
+                  >
+                    <nav
+                      className={`${styles.MuiBox_root} ${styles.css_1y4j5le}`}
+                    >
+                      <a
+                        className={`${styles.MuiButton_root} ${styles.MuiButton_outlined} ${styles.MuiButton_outlinedPrimary} ${styles.MuiButton_sizeMedium} ${styles.MuiButton_outlinedSizeMedium} ${styles.MuiButton_fullWidth} ${styles.MuiButtonBase_root} ${styles.css_1215evx}`}
+                        tabindex="0"
+                        href="/account/create"
+                      >
+                        <span
+                          className={`${styles.MuiTypography_root} ${styles.MuiTypography_button1} ${styles.css_b9fb38}`}
+                        >
+                          Back
+                        </span>
+                      </a>
+                      <button
+                        className={`${styles.MuiButton_root} ${styles.MuiButton_contained} ${styles.MuiButton_containedPrimary} ${styles.MuiButton_sizeMedium} ${styles.MuiButton_containedSizeMedium} ${styles.MuiButton_fullWidth} ${styles.MuiButtonBase_root} ${styles.css_1q2wf90}`}
+                        tabindex="0"
+                        type="submit"
+                      >
+                        <span
+                          className={`${styles.MuiTypography_root} ${styles.MuiTypography_button1} ${styles.css_b9fb38}`}
+                        >
+                          Finish
+                        </span>
+                      </button>
+                    </nav>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </main>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Stage7th;
